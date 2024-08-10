4ab0a6af9e2649531370b6418281502d336a070e
EiEddie <eieddie4u@outlook.com>
2024-08-10 14:42:16 +0800

    feat: 检查传入参数

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
 #[macro_use]
 extern crate rocket;
 
+// TODO: 快速测试
+#[allow(unused)]
+type Result<T> = std::result::Result<T, String>;
+
+/// 检查日期是否正确
+///
+/// - 检查月份是否存在: [1, 12]
+fn check_date(month: u8, year: i32) -> Result<()> {
+	if month < 1 || month > 12 {
+		return Err(format!("`{month}` is not an available month."));
+	}
+
+	// TODO: 检查年份
+	let _year = year;
+
+	Ok(())
+}
+
+/// 检查时间偏移量是否允许
+///
+/// 偏移量大小应该小于等于 12hours, 允许正负
+fn check_offset(offset: i8) -> Result<()> {
+	if offset < -12 || offset > 12 {
+		return Err(format!("`{offset}` is not a allowed offset."));
+	}
+
+	Ok(())
+}
+
 #[get("/dates?<month>&<year>")]
-fn dates(month: u8, year: i32) -> String {
-	format!("{year}-{month:02}")
+fn dates(month: u8, year: i32) -> Result<String> {
+	check_date(month, year)?;
+	Ok(format!("{year}-{month:02}"))
 }
 
 #[get("/fixed_dates?<offset>&<month>&<year>")]
-fn fixed_dates(offset: i8, month: u8, year: i32) -> String {
-	format!("{year}-{month:02} [{offset:+02}]")
+fn fixed_dates(offset: i8, month: u8, year: i32) -> Result<String> {
+	check_date(month, year)?;
+	check_offset(offset)?;
+	Ok(format!("{year}-{month:02} [{offset:+02}]"))
 }
 
 
